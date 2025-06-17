d789edccb1bac6c0a33351d0c0a349a73744dab8
Chris Sinco <csinco@google.com>
2025-06-16 20:32:50 -0700

    Initial Commit

---
+++ chatcat/chatcatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatcatApp.swift
+//  chatcat
+//
+//  Created by Chris Sinco on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct chatcatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
